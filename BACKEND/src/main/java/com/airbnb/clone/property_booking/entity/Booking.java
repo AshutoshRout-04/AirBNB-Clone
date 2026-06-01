6ab5aeae482e7a61113351e0c4ef93908cc33db4
asifhasan169 <Ashifhasan9304@gmail.com>
2026-06-02 01:34:52 +0530

    PROPERTY-Booking Entity

---
+++ BACKEND/src/main/java/com/airbnb/clone/property_booking/entity/Booking.java
@@ -0,0 +1,39 @@
+package com.airbnb.clone.property_booking.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "bookings")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    private Long propertyId;
+
+    private Long userId;
+
+    private LocalDate checkInDate;
+
+    private LocalDate checkOutDate;
+
+    private Double totalPrice;
+
+    private String status;
+
+    private LocalDateTime createdAt;
+}
